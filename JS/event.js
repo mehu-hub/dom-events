8ea5b9411f6319c1bd9e93b5ca38ff88a69b388f
Mehedi Hasan <designwithmehedi@gmail.com>
2023-10-23 22:26:21 +0600

    More Onlick Function

--- JS/event.js
+++ JS/event.js
@@ -1,19 +1,27 @@
 function makeRed() {
     document.body.style.backgroundColor = 'red';
 }
-
 //----------------------------------------------
-
 const makeBlueButton = document.getElementById('make-blue');
 makeBlueButton.onclick = makeBlue;
 
 function makeBlue() {
     document.body.style.backgroundColor = 'blue';
 }
-
 //----------------------------------------------
 const makePurpleButton = document.getElementById('make-purple');
 
 makePurpleButton.onclick = function makePurple() {
     document.body.style.backgroundColor = 'purple';
 }
+//-----------------------------------------------
+const orangeBtn = document.getElementById('make-orange');
+orangeBtn.addEventListener('click', makeOrange)
+
+function makeOrange() {
+    document.body.style.backgroundColor = 'orange';
+}
+//------------------------------------------------ 
+document.getElementById('make-green').addEventListener('click', function () {
+    document.body.style.backgroundColor = 'green';
+})
